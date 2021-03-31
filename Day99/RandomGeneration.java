3d9e82fc34069133d675a1ade5197a9bda6d3f64
Gowtham Vuppala <gowthamv@buffalo.edu>
2021-03-30 21:51:27 -0400

    Added Day 99 files

---
+++ Day99/RandomGeneration.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class RandomGeneration{
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        int k = 5;
+        System.out.println(Arrays.toString(generateKNumbers(nums, k)));
+    }
+
+    public static int[] generateKNumbers(int[] nums, int k){
+        int n = nums.length;
+        Random rand = new Random();
+        int[] res = new int[k];
+        int i = 0;
+        for (;i < k;++i){
+            res[i] = nums[i];
+        }
+
+        for (; i < n; ++i){
+            int j = rand.nextInt(i+1);
+            if (j < k){
+                res[j] = nums[i];
+            }
+        }
+
+        return res;
+    }
+}
